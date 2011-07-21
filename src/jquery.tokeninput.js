f7a8e951c3d680b1d75de2ccaca33cf591a66ca6
Corey Sherman <csherman4@gmail.com>
2011-07-20 22:25:33 -0400

    fixes ie bug

--- src/jquery.tokeninput.js
+++ src/jquery.tokeninput.js
@@ -370,13 +370,11 @@ $.TokenList = function (input, url_or_data_or_function, settings) {
         unique_counter++;
         return 'u' + unique_counter;
     }
-    
-    // hides original input box
-    input.type = 'hidden';
-    
+
     // Keep a reference to the original input box
     var hidden_input = $(input)
                            .val("")
+                           .hide()
                            .focus(function () {
                                input_box.focus();
                            })
